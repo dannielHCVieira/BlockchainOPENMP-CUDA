07ec9856adb76bc79542aef89bb2096a7bddc8bd
dannielHCVieira <danniel.hcvieira@gmail.com>
2022-12-14 19:00:12 -0300

    Added CUDA

---
+++ classes/cuda/Block.cu
@@ -0,0 +1,143 @@
+//
+// Created by Dave Nash on 20/10/2017.
+//
+
+#include "../../headers/cuda/Block.cuh"
+#include "../../headers/cuda/sha256_CPU.cuh"
+
+#define SOLUTION_LEN 25
+
+__constant__ char* characterSet[63] = { "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890" };
+
+Block::Block(uint32_t nIndexIn, const string &sDataIn) : _nIndex(nIndexIn), _sData(sDataIn)
+{
+    _sNonce = '0';
+    _tTime = time(nullptr);
+
+    sHash = _CalculateHash();
+}
+
+// OpenMP (GPU)
+void Block::MineBlock(uint32_t nDifficulty)
+{
+
+    /**
+     * @brief 
+     * Para paralelizar o código da Blockchain utilizando CUDA na GPU, precisamos localizar a parte mais pesada do programa, que é 
+     * justamente a mineração do bloco, mais especificamente no calculo do hash SHA256 que é feito até chegar em uma solução. Por isso,
+     * vamos paralelizar esta parte. Para isso, a ideia que temos é de alocar um bloco na GPU para receber uma string que será utilizada 
+     * na criptografia, um bloco onde estará contido o resultado da criptografia e um bloco para um valor booleano que será utilizado para 
+     * checar se o resultado bate com a string original. Após encontrar a solução em GPU, que deve ser mais ágil que em CPU, devolveremos a 
+     * resposta para a CPU que ira utilizar para criar o novo bloco.
+     */
+    stringstream str_stream;
+    str_stream << _nIndex << _tTime << sPrevHash;
+
+    string str_stream_str = str_stream.str();
+
+    char* input_string = (char*)str_stream_str.c_str();
+    char* d_input;
+
+    char* block_solution = (char*)malloc(sizeof(char) * SOLUTION_LEN);
+    char* d_solution;
+
+    bool* block_isSolved = (bool*)malloc(sizeof(bool));
+    bool* d_isSolved;
+
+    //Alocação da memoria em GPU e copia para da string de input para a mesma
+    cudaMalloc(&d_input, sizeof(char) * str_stream_str.length());
+    cudaMemcpy(d_input, input_string, sizeof(char) * str_stream_str.length(), cudaMemcpyHostToDevice);
+    
+    //Alocação da memoria em GPU e copia da string que conterá a solução
+    cudaMalloc(&d_solution, sizeof(char) * SOLUTION_LEN);
+
+    //Alocação da memoria em GPU para o booleano
+    cudaMalloc(&d_isSolved, sizeof(bool));
+
+    //Seta o solução checker como falso inicialmente
+    unsigned long long seed = static_cast<unsigned long long>(time(nullptr));
+    createSolutionChecker<<<1,1>>>(d_isSolved);
+
+    //Usa o SHA256 paralelizado para chegar na solução
+    bool solution = false;
+    while(!solution) {
+        generateRngSeed_CPU(&seed);
+
+        SHA256_CUDA<<<dimGrid, dimBlock>>>(d_input, d_solution, d_isSolved, nDifficulty, seed, str_stream_str.length());
+        
+        cudaDeviceSynchronize();
+
+        cudaMemcpy(block_isSolved, d_isSolved, sizeof(int), cudaMemcpyDeviceToHost);
+
+        if(*block_isSolved){
+            cudaMemcpy(block_solution, d_solution, sizeof(char) * SOLUTION_LEN, cudaMemcpyDeviceToHost);
+            solution = true;
+
+            break;
+        }
+    }
+
+    cudaDeviceReset();
+
+    _sNonce = string((const char*)block_solution);
+    sHash = _CalculateHash();
+    
+    cout << "Block mined: " << sHash << endl;
+}
+
+__global__ void createSolutionChecker(bool* block_isSolved){
+    *block_isSolved = false;
+}
+
+__global__ void SHA256_CUDA(char* input_string, char* solution, bool* isSolved,uint32_t nDifficulty,unsigned long long seed, size_t text_len){
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned long long newSeed = seed;
+    newSeed = (unsigned long long) i + newSeed;
+
+    char digest[32], random[SOLUTION_LEN];
+
+    memset(digest, 0, SHA256::DIGEST_SIZE);
+    memset(random, 0, SOLUTION_LEN);
+
+
+    for(int j = 0; j < SOLUTION_LEN; j++){
+        newSeed = generateRngSeed_GPU(newSeed);
+        int randomIdx = (int)(newSeed % 62);
+        random[j] = setOfCharacter[randomIdx];
+    }
+
+    SHA256GPU::sha256(input_string, random, text_len, SOLUTION_LEN, digest);
+
+    for (int j = 0; j < nDifficulty; j++){
+        if(digest[j] > 0){
+            return;
+        }
+    }
+
+    if(*block_isSolved){
+        return;
+    }
+
+    *block_isSolved = true;
+    
+    for(int j = 0; j < SOLUTION_LEN; j++){
+        solution[j] = random[j];
+    }
+}
+
+void Block::generateRngSeed_CPU(unsigned long long* x)
+{
+    *x ^= (*x << 21);
+    *x ^= (*x >> 35);
+    *x ^= (*x << 4);
+}
+
+inline string Block::_CalculateHash() const
+{
+    stringstream ss;
+    ss << _nIndex << sPrevHash << _tTime << _sData << _nNonce;
+    
+    return sha256(ss.str());
+}
+
+
---
+++ main.cu
@@ -0,0 +1,31 @@
+#include <stdbool.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include "headers/cuda/Blockchain.cuh"
+
+bool is_string_empty(char* c) {
+    return ((c != NULL) && (c[0] == '\0'));
+}
+
+int main(int argc, char** argv)
+{
+    int num_blocks = 3, difficulty = 2;
+
+    if (!is_string_empty(argv[1]))
+        num_blocks = atoi(argv[1]);
+    if (!is_string_empty(argv[2]))
+        difficulty = atoi(argv[2]);
+
+    cout << "Num blocks: " << num_blocks << endl;
+    cout << "Difficulty: " << difficulty << endl << endl;
+
+    Blockchain bChain = Blockchain(difficulty);
+
+    for(int i = 0; i < num_blocks; i++ ) {
+        cout << "Mining block " << i + 1 << "..." << endl;
+        bChain.AddBlock(Block(i+1, "Block "+std::to_string(i+1)+" Data"));
+    }
+
+    return 0;
+}
